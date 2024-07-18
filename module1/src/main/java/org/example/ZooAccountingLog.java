ce198061e54dc0a4a82861c5c7b63773789fc084
Alisa Negrobova <20alisa03@gmail.com>
2024-07-18 16:03:02 +0300

    Реализация задачи

---
+++ module1/src/main/java/org/example/ZooAccountingLog.java
@@ -0,0 +1,74 @@
+package org.example;
+
+
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ZooAccountingLog {
+
+    // Животное, которое съело больше всего корма за последний месяц
+    public Optional<AnimalClass> findAnimalMostEatLastMonth(List<Entry> entryList) {
+
+        LocalDate lastMonth = LocalDate.now().minusMonths(1);
+
+        Map<AnimalClass, Double> fatAnimal =
+                entryList.stream()
+                        .filter(entry -> entry.getDate().isAfter(lastMonth))
+                        .collect(Collectors.groupingBy(Entry::getAnimalClass, Collectors.summingDouble(Entry::getFoodWeight)));
+
+        return fatAnimal.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey);
+    }
+
+    //  Найти месяц, в который животные получают самое разнообразные питание
+    public static List<Month> monthMostDiferenseEat(List<Entry> entryLists) {
+
+        Map<Month, Set<FoodList>> monthWithEat = new HashMap<>();
+        for(Month month : Month.values()) {
+            monthWithEat.put(month, new HashSet<>());
+        }
+        entryLists.forEach(entryList -> entryList.getFoodList().forEach(foodlist -> monthWithEat.get(entryList.getDate().getMonth())
+                .add(foodlist)));
+        int maxCountEat = monthWithEat.values().stream()
+                .map(Set::size)
+                .max(Comparator.naturalOrder())
+                .orElseThrow();
+        return monthWithEat.entrySet().stream()
+                .filter(month -> month.getValue().size() == maxCountEat)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+    // Для каждого животного, найти продукт, которое оно ело в прошлом месяце, но не ело в этом
+    public static Map<AnimalClass, Set<String>> findUnconsumedFoods(List<Entry> entries) {
+        LocalDate today = LocalDate.now();
+        LocalDate firstDayOfCurrentMonth = today.withDayOfMonth(1);
+        LocalDate firstDayOfPreviousMonth = firstDayOfCurrentMonth.minusMonths(1);
+
+        Map<Boolean, List<Entry>> partitionedEntries = entries.stream()
+                .collect(Collectors.partitioningBy(e -> e.getDate().isBefore(firstDayOfCurrentMonth) && !e.getDate().isBefore(firstDayOfPreviousMonth)));
+
+        List<Entry> previousMonthEntries = partitionedEntries.get(true);
+        List<Entry> currentMonthEntries = partitionedEntries.get(false);
+
+        Map<AnimalClass, List<FoodList>> previousMonthFoodLists = previousMonthEntries.stream()
+                .collect(Collectors.groupingBy(Entry::getAnimalClass, Collectors.flatMapping(entry -> entry.getFoodList().stream(), Collectors.toList())));
+
+        Map<AnimalClass, List<FoodList>> currentMonthFoodLists = currentMonthEntries.stream()
+                .collect(Collectors.groupingBy(Entry::getAnimalClass, Collectors.flatMapping(entry -> entry.getFoodList().stream(), Collectors.toList())));
+
+        Map<AnimalClass, Set<String>> unconsumedFoods = new HashMap<>();
+        for (AnimalClass animalClass : previousMonthFoodLists.keySet()) {
+            Set<String> previousMonthFoods = previousMonthFoodLists.get(animalClass).stream().map(FoodList::getFoodListName).collect(Collectors.toSet());
+            Set<String> currentMonthFoods = currentMonthFoodLists.getOrDefault(animalClass, Collections.emptyList()).stream().map(FoodList::getFoodListName).collect(Collectors.toSet());
+
+            previousMonthFoods.removeAll(currentMonthFoods);
+            unconsumedFoods.put(animalClass, previousMonthFoods);
+        }
+
+        return unconsumedFoods;
+    }
+}
